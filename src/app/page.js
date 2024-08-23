a2ed2ea3436931f109c17d22059371b95a1626fe
luismagnum <luismagnum1@gmail.com>
2024-08-23 00:48:16 -0300

    second

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect} from 'react';
+import { useEffect} from 'react';
 import AOS from "aos";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
